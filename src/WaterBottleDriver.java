f271fe98720c0eeb87a32c101c9f6edbf61194eb
mrkallay <brian.kallay@gmail.com>
2021-09-07 13:46:12 -0700

    added driver for waterbottle

---
+++ src/WaterBottleDriver.java
@@ -0,0 +1,20 @@
+/*
+Created by Brian Kallay
+*/
+
+public class WaterBottleDriver
+{
+    public static void main(String[] args)
+    {
+        int x = 5;
+        boolean y = false;
+        WaterBottle myBottle = new WaterBottle(64, 32.5);
+        String currentStatus = myBottle.getInfo();
+        System.out.println(currentStatus);
+
+        WaterBottle anotherBottle = new WaterBottle(100);
+        System.out.println(anotherBottle.getInfo());
+        new WaterBottle(56, 4);
+
+    }
+}
