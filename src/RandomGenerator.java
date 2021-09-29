f96dcc635b1899bdf982fcda3ea54e80c2915ce6
mrkallay <brian.kallay@gmail.com>
2021-09-29 08:55:46 -0700

    completed

--- src/RandomGenerator.java
+++ src/RandomGenerator.java
@@ -4,8 +4,8 @@ Created by Brian Kallay
 
 public class RandomGenerator
 {
-    int min;
-    int max;
+    private int min;
+    private int max;
 
     public RandomGenerator()
     {
@@ -15,33 +15,24 @@ public class RandomGenerator
 
     //a )Create a second constructor that accepts a min and max as parameters and apply those
     //values to the appropriate instance variables (3 pts)
-
-
-
-
-
-
-
+    public RandomGenerator(int min, int max)
+    {
+        this.min = min;
+        this.max = max;
+    }
 
     //b ) Complete the following getter method so that it returns the value of the instance
     //    variable max to the caller (1 pt)
     public int getMax()
     {
-
-
-
+        return max;
     }
 
     //3)complete the following method that is intended to return a random int
-    //that is between min and max inclusive (4 pts)
-    public void getRandomInt()
+    //that is between min and max inclusive (2 pts)
+    public int getRandomInt()
     {
-
-
-
-
-
-
-
+        int random = min +  (int)(Math.random() * (max - min +1 ));
+        return random;
     }
 }
--- src/RandomGeneratorDriver.java
+++ src/RandomGeneratorDriver.java
@@ -7,10 +7,11 @@ public class RandomGeneratorDriver
     public static void main(String[] args)
     {
         // a) instantiate a RandomGenerator object with min = 50 and max = 200 (1pt)
-
+        RandomGenerator rand = new RandomGenerator(50, 200);
         // b) create a rondom int using the getRandomInt method from the RandomGenerator class
         //and assign it to a variable named random (1pt)
-
+        int random = rand.getRandomInt();
         // c) print out the value of the random variable (1pt)
+        System.out.println(random);
     }
 }
