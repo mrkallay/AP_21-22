8084de636122f70b3053b6f60a3fd33eb6443e9a
mrkallay <brian.kallay@gmail.com>
2021-09-29 08:08:30 -0700

    driver commit

---
+++ src/RandomGeneratorDriver.java
@@ -0,0 +1,16 @@
+/*
+Created by Brian Kallay
+*/
+
+public class RandomGeneratorDriver
+{
+    public static void main(String[] args)
+    {
+        // a) instantiate a RandomGenerator object with min = 50 and max = 200 (1pt)
+
+        // b) create a rondom int using the getRandomInt method from the RandomGenerator class
+        //and assign it to a variable named random (1pt)
+
+        // c) print out the value of the random variable (1pt)
+    }
+}
