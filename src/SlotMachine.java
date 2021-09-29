e22d2d32088e3d89827782241cd4991c29d2f53d
mrkallay <brian.kallay@gmail.com>
2021-09-29 14:50:40 -0700

    initial version;

---
+++ src/SlotMachine.java
@@ -0,0 +1,34 @@
+/*
+Created by Brian Kallay
+*/
+
+public class SlotMachine
+{
+    private int low;
+    private int high;
+
+    public SlotMachine()
+    {
+        low = 1;
+        high = 9;
+    }
+    //a )Create a second constructor that accepts low & high as parameters and apply the
+    //   value to the instance variable
+
+    //b )Implement the setHigh() method below so that the value of the parameter is applied
+    //   to the appropriate instance variable
+    public void setHigh(int high)
+    {
+
+
+    }
+
+    //c ) Implement the spinIt method which is intended to print 3 random integers between
+    //    low and high. This method should not return anything.
+    //    Example output of calling spinIt would be:
+    //        735
+    public void spinIt()
+    {
+
+    }
+}
